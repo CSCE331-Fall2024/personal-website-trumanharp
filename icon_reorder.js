6515926fd9ef7d27e6e48892f3708f8ee4d7e959
trumanharp-tamu <trumanharp@tamu.edu>
2024-09-12 17:33:29 -0500

    finish most of style2

--- icon_reorder.js
+++ icon_reorder.js
@@ -2,5 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var iconBar = document.getElementById("icon_bar");
     var qualContent = document.getElementById("qual_content");
 
-    qualContent.parentNode.insertBefore(iconBar,qualContent);
+    if (iconBar && qualContent) {
+        qualContent.parentNode.insertBefore(iconBar,qualContent);
+    }
+
+    
 });
