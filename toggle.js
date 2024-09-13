dd0c8ba766f20260c905fa7a5c99e04eba7f6dd2
trumanharp-tamu <trumanharp@tamu.edu>
2024-09-12 19:03:27 -0500

    finished the switch and added more links. also got up to 300 words in portfolio

--- toggle.js
+++ toggle.js
@@ -4,10 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
     var alternateCSS = document.getElementById("alternate");
 
     function toggle() {
-        defaultCSS.disabled = styleToggle.checked;
-        alternateCSS.disabled = !styleToggle.checked;
+        if(styleToggle.checked) {
+            defaultCSS.disabled = true;
+            alternateCSS.disabled = false;
+            localStorage.setItem("css", "alt");
+        } else {
+            defaultCSS.disabled = false;
+            alternateCSS.disabled = true;
+            localStorage.setItem("css", "default");
+        }
+    }
+    var saved = localStorage.getItem("css");
+
+    if(saved == "alt") {
+        styleToggle.checked = true;
+        /* defaultCSS.disabled = true;
+        alternateCSS.disabled = false; */
+    } else {
+        styleToggle.checked = false;
+        /* defaultCSS.disabled = false;
+        alternateCSS.disabled = true; */
     }
 
     toggle();
     styleToggle.addEventListener("change", toggle);
-})
+});
